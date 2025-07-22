94623bdd9e6047e77f14707df78cf870b3b37b37
elfacu0 <facunk4@gmail.com>
2025-07-22 09:52:40 -0600

    Rename function

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,10 +1,8 @@
 import { Canvas } from "@/components/Canvas";
 
-export default function HomePage() {
-
+export default function EditorPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold mb-8">Fabric Canvas Example</h1>
       <Canvas />
     </main>
   );
