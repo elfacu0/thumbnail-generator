ef50f96b9d8b1a8a06e749e05fca17e66deb111f
elfacu0 <facunk4@gmail.com>
2025-07-24 08:33:18 -0600

    Generate iamge from canvas

--- src/hooks/useEditor.ts
+++ src/hooks/useEditor.ts
@@ -5,14 +5,29 @@ import { generateImagePrompt } from "@/utils/generatePrompt";
 export const useEditor = () => {
   const { canvasRef, addElement, getElements } = useFabric();
 
-  const convertImage = () => {
+  const generateImageFromCanvas = async () => {
     const fabricElements = getElements();
     const elements = fromFabricElementsToElements(fabricElements);
-    const prompt = generateImagePrompt(elements)
+    const prompt = generateImagePrompt(elements);
+
+    const response = await fetch("/api/ai/images", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to generate image");
+    }
+
+    const data = await response.json();
+    return data.image;
   };
 
   return {
-    convertImage,
+    generateImageFromCanvas,
     canvasRef,
     addElement,
   };
